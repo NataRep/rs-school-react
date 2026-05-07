67fe2e43ca7d5f0da38a1b5141ec4cd13e45e8f2
Natalia Repkina <pn.88@mail.ru>
2026-05-07 12:39:13 +0300

    feat: add tests for Button

--- src/components/shared/button/Button.spec.tsx
+++ src/components/shared/button/Button.spec.tsx
@@ -1,5 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Button from './Button';
+
+jest.mock('../icon/Icon', () => {
+  interface MockIconProps {
+    name: string;
+  }
+
+  return function MockIcon({ name }: MockIconProps) {
+    return <span data-testid="mock-icon">{name}</span>;
+  };
+});
 
 describe('Button component', () => {
   it('should render the button with correct text', () => {
@@ -18,4 +28,61 @@ describe('Button component', () => {
 
     expect(buttonElement).toBeInTheDocument();
   });
+
+  it('should called callback function', () => {
+    const buttonText = 'Click me';
+    const mockCallback = jest.fn();
+
+    render(
+      <Button
+        text={buttonText}
+        callback={mockCallback}
+        disabled={false}
+      />
+    );
+    const buttonElement = screen.getByRole('button', { name: /click me/i });
+
+    fireEvent.click(buttonElement)
+
+    expect(mockCallback).toHaveBeenCalled()
+  });
+
+  it('should render icon', () => {
+
+    const buttonText = 'Click me';
+    const mockCallback = jest.fn();
+    const iconName = 'search'
+
+    render(
+      <Button
+        text={buttonText}
+        callback={mockCallback}
+        disabled={false}
+        icon={iconName}
+      />
+    );
+
+    const iconElement = screen.getByTestId('mock-icon');
+
+    expect(iconElement).toBeInTheDocument();
+    expect(iconElement).toHaveTextContent(iconName);
+  })
+
+  it('should not render icon without prop', () => {
+
+    const buttonText = 'Click me';
+    const mockCallback = jest.fn();
+
+    render(
+      <Button
+        text={buttonText}
+        callback={mockCallback}
+        disabled={false}
+      />
+    );
+
+    const iconElement = screen.queryByTestId('mock-icon')
+
+    expect(iconElement).toBeNull();
+  })
 });
