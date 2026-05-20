f7c251616c917626973a68528ef05d03251d4a19
Natalia Repkina <pn.88@mail.ru>
2026-05-20 10:08:56 +0300

    ref: select router in single file

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,7 @@
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
-import About from './components/about/About';
-import Layout from './components/layout/Layout';
-import { NotFoundPage } from './components/not-found-page/NotFoundPage';
-import Search from './components/search/Search';
-import DetailView from './components/search/detail/Detail';
-import { detailLoader } from './components/search/detail/detailLoader';
-import SearchResult from './components/search/search-result/SearchResult';
-import { searchResultLoader } from './components/search/search-result/searchLoader';
-import ErrorBoundary, { RouterErrorCatch } from './components/shared/error-boundary/ErrorBoundary';
-import Loader from './components/shared/loader/Loader';
+import { RouterProvider } from 'react-router-dom';
+import ErrorBoundary from './components/shared/error-boundary/ErrorBoundary';
+import { router } from './Router';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Layout />,
-      HydrateFallback: () => <Loader />,
-      errorElement: <RouterErrorCatch />,
-      children: [
-        {
-          index: true,
-          element: <Navigate to="/search/people" replace />,
-        },
-        {
-          path: 'search',
-          element: <Search />,
-          children: [
-            {
-              index: true,
-              element: <Navigate to="people" replace />,
-            },
-            {
-              path: ':categoryName',
-              element: <SearchResult />,
-              HydrateFallback: () => <Loader />,
-              loader: searchResultLoader,
-              shouldRevalidate: ({ currentParams, nextParams, currentUrl, nextUrl }) => {
-                if (currentParams.categoryName !== nextParams.categoryName) return true;
-                if (currentUrl.search !== nextUrl.search) return true;
-
-                return false;
-              },
-              children: [
-                {
-                  path: ":id",
-                  element: <DetailView />,
-                  loader: detailLoader,
-                }
-              ]
-            },
-          ],
-        },
-        {
-          path: 'about',
-          element: <About />,
-        }
-      ],
-    },
-    { path: "*", element: <NotFoundPage /> }
-  ],
-  {
-    basename: process.env.NODE_ENV === 'test' ? '/' : '/rs-school-react',
-  }
-);
 
 export default function App() {
   return <ErrorBoundary>
