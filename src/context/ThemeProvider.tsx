7ce095e928e33917e456d76d1999da33ab481893
Natalia Repkina <pn.88@mail.ru>
2026-05-22 14:43:32 +0300

    feat: add save theme after reload

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,9 +1,23 @@
-import { useState, type ReactNode } from "react";
+import { useEffect, useState, type ReactNode } from "react";
 import { ThemeContext, type Theme } from "./ThemeContext";
 
+const LOCAL_STORAGE_THEME_KEY = 'star-wars-user-theme';
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useState<Theme>('dark');
+
+  const [theme, setTheme] = useState<Theme>(() => {
+    const savedTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY);
+
+    if (savedTheme === 'light' || savedTheme === 'dark') {
+      return savedTheme;
+    }
+
+    return 'dark';
+  });
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, theme);
+  }, [theme]);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -4,5 +4,9 @@ import { ThemeContext } from "../context/ThemeContext";
 export const useTheme = () => {
   const context = useContext(ThemeContext);
 
+  if (context === undefined) {
+    throw new Error('`useTheme` must be used strictly within `ThemeProvider`');
+  }
+
   return context;
 };
