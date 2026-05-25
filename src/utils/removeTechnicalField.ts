8a1e2157e0c5279e50b327bd539318b90a1fea5a
Natalia Repkina <pn.88@mail.ru>
2026-05-25 12:18:25 +0300

    fix: change removeTechnicalFields

--- src/utils/removeTechnicalField.ts
+++ src/utils/removeTechnicalField.ts
@@ -13,7 +13,8 @@ export function removeTechnicalFields(obj: Record<string, unknown>): [string, st
 
   const filteredFieldsArray = Object.entries(obj)
     .filter((pair): pair is [string, string | number] => {
-      const [, value] = pair;
+      const [key, value] = pair;
+      if (key.toLowerCase() === "created" || key.toLowerCase() === "edited") return false;
       if (typeof value === 'string' && !value.startsWith(technicalStart)) return true;
       if (typeof value === 'number') return true;
       return false;
