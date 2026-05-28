8dfc8c06d05aff0d44bea68559a0e3923390d14c
Natalia Repkina <pn.88@mail.ru>
2026-05-28 12:05:52 +0300

    feat: add starWarsApi

---
+++ src/store/starWarsApi.ts
@@ -0,0 +1,48 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { Person, Planet, Starship } from "../services/api-service/api-models";
+
+export type CategoryMap = {
+  people: Person;
+  planets: Planet;
+  starships: Starship;
+};
+
+export const starWarsApi = createApi({
+  reducerPath: 'starWarsApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://swapi.py4e.com/api',
+    prepareHeaders: (headers) => {
+      headers.set('Accept', 'application/json');
+      return headers;
+    },
+
+  }),
+
+  keepUnusedDataFor: Number(process.env.VITE_CACHE_TTL) || 60,
+  tagTypes: ['SwapiData'],
+
+  endpoints: (builder) => ({
+    getData: builder.query({
+      query: ({ category, searchQuery, page = 1 }) => {
+        const searchParams = searchQuery ? `&search=${encodeURIComponent(searchQuery)}` : '';
+        return {
+          url: `${category}/?page=${page}${searchParams}`,
+          method: 'GET',
+        };
+      },
+      providesTags: (_, __, arg) => [
+        { type: 'SwapiData', id: arg.category }
+      ],
+    }),
+
+    getEntityDetails: builder.query({
+      query: ({ category, id }) => ({
+        url: `${category}/${id}`,
+        method: 'GET',
+      }),
+      providesTags: (_, __, arg) => [
+        { type: 'SwapiData', id: `${arg.category}_${arg.id}` }
+      ],
+    }),
+  }),
+});
