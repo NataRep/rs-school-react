591a2c8a1d5a86800be88b8ad1fa9353ec9372e6
Natalia Repkina <pn.88@mail.ru>
2026-06-08 15:42:42 +0300

    feat: add testing dependencies

---
+++ rs-react-app/jest.config.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
+    '\\.svg(\\?react)?$': '<rootDir>/src/__mocks__/svgMock.tsx',
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.tsx',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/*models.ts',
+    '!src/main.tsx',
+    '!src/index.tsx',
+    '!src/setupTests.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 50,
+      functions: 50,
+      lines: 50,
+    },
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.jest.json',
+      },
+    ],
+  },
+};
