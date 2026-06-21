af9a64675783c18f9e9f18aee997d5e4170fd8e3
Natalia Repkina <pn.88@mail.ru>
2026-06-21 14:21:56 +0300

    feat: add next-intl middleware for automatic locale redirect

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import createMiddleware from 'next-intl/middleware';
+import { localePrefix, locales } from './i18n/navigation';
+
+export default createMiddleware({
+  locales: locales,
+  defaultLocale: 'en',
+  localePrefix: localePrefix,
+});
+
+export const config = {
+  matcher: ['/', '/(ru|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)'],
+};
