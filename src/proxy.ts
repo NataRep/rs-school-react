1e795eaa485d5d1d44ae0f8cb6fb1ff8022a5e9e
Natalia Repkina <pn.88@mail.ru>
2026-06-22 16:39:58 +0300

    fix: update middleware  matcher rules to prevent asset and data blocking

--- src/proxy.ts
+++ src/proxy.ts
@@ -8,5 +8,5 @@ export default createMiddleware({
 });
 
 export const config = {
-  matcher: ['/', '/(ru|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)'],
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)'],
 };
